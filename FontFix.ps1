4f3a658914417c6c74bc8c1a1491b90767514179
Khalil Hasanzade <112690645+hasanzadekhalil@users.noreply.github.com>
2023-02-22 16:01:05 +0400

    Add files via upload

---
+++ FontFix.ps1
@@ -0,0 +1,12 @@
+﻿$WordFontUrl = “https://github.com/hasanzadekhalil/FontChangeOffice/raw/main/Normal.dotm”
+$OutlookFontFix = “https://raw.githubusercontent.com/hasanzadekhalil/FontChangeOffice/main/Outlook.reg”
+
+$WordFontPath=”C:\Temp\Normal.dotm”
+$OutlookFontPath="C:\Temp\Outlook.reg"
+
+Invoke-WebRequest -URI $WordFontUrl -OutFile $WordFontPath
+Invoke-WebRequest -URI $OutlookFontFix -OutFile $OutlookFontPath
+
+regedit.exe /s $OutlookFontPath
+
+Copy-Item $WordFontPath -Destination "$env:APPDATA\Microsoft\Templates"
