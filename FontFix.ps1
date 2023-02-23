bc20ee190831647611d6a155d8ba05f346794e77
Khalil Hasanzade <112690645+hasanzadekhalil@users.noreply.github.com>
2023-02-23 15:58:00 +0400

    Add files via upload

--- FontFix.ps1
+++ FontFix.ps1
@@ -1,4 +1,12 @@
-﻿$WordFontUrl = “https://github.com/hasanzadekhalil/FontChangeOffice/raw/main/Normal.dotm”
+﻿if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $Command = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
+        Exit
+ }
+}
+
+$WordFontUrl = “https://github.com/hasanzadekhalil/FontChangeOffice/raw/main/Normal.dotm”
 $OutlookFontFix = “https://raw.githubusercontent.com/hasanzadekhalil/FontChangeOffice/main/Outlook.reg”
 
 $WordFontPath=”C:\Temp\Normal.dotm”
@@ -10,3 +18,4 @@ Invoke-WebRequest -URI $OutlookFontFix -OutFile $OutlookFontPath
 regedit.exe /s $OutlookFontPath
 
 Copy-Item $WordFontPath -Destination "$env:APPDATA\Microsoft\Templates"
+
